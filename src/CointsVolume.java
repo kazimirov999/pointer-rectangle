6934cd389a543ddf974a4fead66a6240164c77f1
nikolaenko92 <59179849+nikolaenko92@users.noreply.github.com>
2019-12-26 01:51:18 +0200

    add

---
+++ src/CointsVolume.java
@@ -0,0 +1,17 @@
+ class CointsVolume {
+    public static void main(String[] args) {
+        int length1 = 2, width1 = 2;
+        Rectangle rectangle1 = new Rectangle(length1, width1);
+        Rectangle rectangle2 = new Rectangle(length1, width1);
+        printRectangleVolume(rectangle1);
+        printRectangleVolume2(rectangle2);
+    }
+
+    static void printRectangleVolume(Rectangle rectangle1) {
+        System.out.println("Площа = " + rectangle1.getVolume());
+    }
+
+    static void printRectangleVolume2(Rectangle rectangle2) {
+        System.out.println("Площа = " + rectangle2.getVolume2());
+    }
+}
