00c72b31188d7881a0906af1c5adff8c94a949c0
nikolaenko92 <59179849+nikolaenko92@users.noreply.github.com>
2019-12-26 01:59:10 +0200

    add all

--- src/CointsVolume.java
+++ src/CointsVolume.java
@@ -1,6 +1,17 @@
- class CointsVolume {
+import java.util.Scanner;
+
+class CointsVolume {
     public static void main(String[] args) {
-        int length1 = 2, width1 = 2;
+
+        Scanner sc = new Scanner(System.in);
+        int length1, width1;
+        System.out.println("Введить довжину");
+        length1 = sc.nextInt();
+
+        System.out.println("Введить ширину");
+        width1 = sc.nextInt();
+
+
         Rectangle rectangle1 = new Rectangle(length1, width1);
         Rectangle rectangle2 = new Rectangle(length1, width1);
         printRectangleVolume(rectangle1);
@@ -12,6 +23,6 @@
     }
 
     static void printRectangleVolume2(Rectangle rectangle2) {
-        System.out.println("Площа = " + rectangle2.getVolume2());
+        System.out.println("Периметр = " + rectangle2.getVolume2());
     }
 }
