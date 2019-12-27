e41ed40a7551fe4d47bd18040af58dde6014bdbe
Vitalii Solodovnyk <vitaliysecretua@gmail.com>
2019-12-27 16:24:20 +0200

    add branch Rectangle

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle myRectangle = new Rectangle(10, 5);
+        myRectangle.getperimeter();
+        myRectangle.getarea();
+}
+}
